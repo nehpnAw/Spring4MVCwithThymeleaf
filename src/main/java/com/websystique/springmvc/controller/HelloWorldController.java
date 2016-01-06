d3ab34c247dfd3df9f1b9d02323586210771f3f7
Wanphen <fullmoon16301@gmail.com>
2016-01-06 11:43:05 +0700

    first commit
    First commit add code to github

---
+++ src/main/java/com/websystique/springmvc/controller/HelloWorldController.java
@@ -0,0 +1,45 @@
+package com.websystique.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+@RequestMapping("/")
+public class HelloWorldController {
+
+	@RequestMapping(method = RequestMethod.GET)
+    public String sayHello( ModelMap model ) {
+		
+        model.addAttribute("greeting", "Hello World from Spring 4 MVC");
+        return "welcome";
+        
+    }
+ 
+    @RequestMapping(value="/helloagain", method = RequestMethod.GET)
+    public String sayHelloAgain(ModelMap model) {
+    	
+        model.addAttribute("greeting", "Hello World Again, from Spring 4 MVC");
+        return "welcome";
+        
+    }
+    
+    @RequestMapping(value="/testSubmit", method = RequestMethod.POST)
+    public String testSubmit( 
+    		@RequestParam("username") String username, 
+    		@RequestParam("password") String password,
+    		@RequestParam("field") String field,
+    		ModelMap model ){
+    	
+    		System.out.println("This username is : " + username + " and this password is : " + password + " " + field);
+    		model.addAttribute("user", username);
+    		model.addAttribute("pass", password);
+    	
+    	return "success";
+    	
+    }
+	
+}
